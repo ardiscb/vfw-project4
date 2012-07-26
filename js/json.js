fe1578f6a2ecdf084239a8f25dcf31c84fa7b45f
Courtney Ardis <ardiscb@fullsail.edu>
2012-07-25 20:46:17 -0400

    Added json data

---
+++ js/json.js
@@ -0,0 +1,41 @@
+/*
+Author: Courtney Ardis 
+Project: Web App Part 4
+Term: 1207
+*/
+
+var json = {
+	"comic1": {
+		"comicTitle": ["Title of Comic:", "Truth..."],
+		"seriesTitle": ["Title of Series:", "Trinity"],
+		"issueNum": ["Issue Number:", "10"],
+		"dateReleased": ["Date Released:", "2008-08-06"],
+		"publisher": ["Publisher:", "DC Comics"],
+		"rateIssue": ["Rate Issue:", "5"],
+		"genre": ["Genre:", "Superhero"],
+		"illStyle": ["Illustration Style:", "Full Color"],
+		"comments": ["Comments:", "Wonder woman cover"]
+	},
+		"comic2": {
+		"comicTitle": ["Title of Comic:", "The Vampire Walks Among Us"],
+		"seriesTitle": ["Title of Series:", "The Tomb of Dracula"],
+		"issueNum": ["Issue Number:", "37"],
+		"dateReleased": ["Date Released:", "1975-10-01"],
+		"publisher": ["Publisher:", "Marvel Comics"],
+		"rateIssue": ["Rate Issue:", "5"],
+		"genre": ["Genre:", "Horror"],
+		"illStyle": ["Illustration Style:", "Full Color"],
+		"comments": ["Comments:", ""]
+	},
+		"comic3": {
+		"comicTitle": ["Title of Comic:", "Conan the Barbarian...were stranded in the 20th Century?"],
+		"seriesTitle": ["Title of Series:", "What If"],
+		"issueNum": ["Issue Number:", "43"],
+		"dateReleased": ["Date Released:", "1984-02-01"],
+		"publisher": ["Publisher:", "Marvel Comics"],
+		"rateIssue": ["Rate Issue:", "5"],
+		"genre": ["Genre:", "Sci-Fi"],
+		"illStyle": ["Illustration Style:", "Full Color"],
+		"comments": ["Comments:", "Meets Captain America"]
+	}
+}
